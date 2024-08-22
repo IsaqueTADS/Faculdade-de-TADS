4278747699174e65d5b48cd5e3748c7d34700e38
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2024-08-22 00:48:44 -0300

    primeiros exercicios vetor e eu usei function

---
+++ 1-periodo/Lista-vetor-1/ex001.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+
+using namespace std;
+
+float temperatura[12];
+int numMeses = 12;
+
+float somaMes(float temperatura[], int numMeses);
+
+int main (){
+
+    
+    for ( int i = 0; i < numMeses ; i ++ ){
+
+        cout<<"Digite a temperatura do mês "<<i+1<<endl;
+        cin>>temperatura[i];
+        
+    }
+    
+    float somaFinal = somaMes(temperatura,numMeses);
+
+    float mediaAnual = somaFinal/numMeses;
+
+    cout<<"Media: "<<mediaAnual<<endl;
+    
+    for ( int j = 0; j < numMeses; j ++){
+        if ( temperatura[j] > mediaAnual){
+            cout<<"Mes: "<<j+1<<" esta acima da media\ntemperatura de: "<<temperatura[j]<<endl;
+        }
+    }
+
+
+
+    return 0;
+}
+
+float somaMes ( float temperatura[], int numMeses){
+    float somaFunc = 0;
+    for ( int i = 0 ; i < numMeses; i++ ){
+
+        somaFunc= somaFunc + temperatura[i];
+
+    }
+    
+    return somaFunc;
+
+}
