c25ef0acca83787a1f196f7c7c21f70ddc36d20a
IsaqueTADS <isaqueofc18@gmail.com>
2025-01-29 20:31:35 -0300

    lista dupla

---
+++ Algoritimo/2-periodo/Algoritimo-de-lista/lista-duplamente-encadneada.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+struct No {
+    int valor;
+    No* anterior;
+    No* proximo;
+};
+
+struct ListaDupla
+{
+    No* primeiroNo;
+    void criar();
+    void inserir(int valor);
+};
+
+
+int main (){
+
+
+
+ return 0;
+}
+
+void ListaDupla::criar(){
+    primeiroNo = NULL;
+}
+
+void ListaDupla::inserir(int valor){
+    No* novoNo = new No;
+    novoNo -> proximo = NULL; 
+    novoNo -> anterior = NULL; 
+    novoNo -> valor = valor;
+
+    if (primeiroNo == NULL){
+        primeiroNo = NULL;
+    }
+    else {
+        No* aux = primeiroNo;
+        while (aux -> proximo != NULL){
+            aux = aux -> proximo;
+        }
+        novoNo -> anterior = aux;
+        aux -> proximo = novoNo;
+    }
+}
